7eb7fb0ceac83f37fa49038f1d6bfe021bcad2c7
derrick b <dbozich1@gmail.com>
2018-06-03 07:55:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 # require_relative './fancy_dance.rb'
 
 class Song
-  # extend Memorable::ClassMethods
+  extend Memorable::ClassMethods
   attr_accessor :name
   attr_reader :artist
 
